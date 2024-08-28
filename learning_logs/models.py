855cb7f5af4b6bf50581f988e13072abcfb2b9a2
Abubakr <110747776+abubakerKhaled@users.noreply.github.com>
2024-08-29 02:33:43 +0300

    😁 Adding the Topic model and the app to the project

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,2 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
+class Topic(models.Model):
+    """A topic the user is learning about."""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        """Returns a string representation of the model."""
+        return self.text

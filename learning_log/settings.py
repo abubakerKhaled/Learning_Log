25095f49656b6b2ef787db277a7755eab6331f0b
Abubakr <110747776+abubakerKhaled@users.noreply.github.com>
2024-09-01 23:52:41 +0300

    ❤️ Adding the styling to the templates making it looks pretty using bootstrap5

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -31,17 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    
-    # Myapps
-    'learning_logs',
-    #Users
-    'users',
+    # Third party apps
+    "django_bootstrap5",
+    # Django apps
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    # My apps
+    "learning_logs",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -120,7 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -130,3 +136,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # My Settings
 LOGIN_URL = '/users/login/'
+
+# Settings for django-bootstrap5
+BOOTSTRAP5 = {
+    'include_jquery': True,
+}
